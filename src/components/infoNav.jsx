71a61e882a49abecde4ca6ad8289e15bb17bd2ab
marcebenitez2 <marcebenitez0607@gmail.com>
2024-01-03 19:43:37 -0300

    Update height of InfoNav component

--- src/components/infoNav.jsx
+++ src/components/infoNav.jsx
@@ -5,7 +5,7 @@ import { ImHome } from "react-icons/im";
 
 function InfoNav() {
   return (
-    <nav className="w-full h-12 flex justify-between items-center px-10 fixed top-0 bg-red-600 text-white font-semibold lgn:h-16 smn:px-3 z-50">
+    <nav className="w-full h-14 flex justify-between items-center px-10 fixed top-0 bg-red-600 text-white font-semibold lgn:h-16 smn:px-3 z-50">
       <div>
         <Link to={"/"}>
           <ImHome size={45} />
